1e31a11466a97a22227bf4d530c4c30a375c632a
Renaud Deschenes <102036597+ZelenskyGigaChad@users.noreply.github.com>
2022-09-28 08:05:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #On crée notre fonction pour exécuter le programme de "comptage" de mots.
 def count_word(phrase):
-#On affiche le nombre de mots
+#On retourne le nombre de mots
     return (len(phrase.split()), "mots")
 
 quitter = ""
 #Tant que l'utilisateur ne quitte pas, on continue le code
 while quitter != "oui":
+#On prend un input en str pour faire activer la fonction count_word, puis on l'affiche
     print(count_word(input("Votre phrase : ")))
 
     quitter = input("Voulez-vous quitter? : ").lower()
